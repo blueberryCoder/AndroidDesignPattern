e2170d1ddaa41f7ede60a67eaa37fe7bc8e54387
blueberry <992036795@qq.com>
2016-03-28 09:44:56 +0800

    delete threadJni

--- ThreadsJNI/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := Threads
-LOCAL_SRC_FILES := Threads.cpp com_example_threads_MainActivity.h
-
-include $(BUILD_SHARED_LIBRARY)
